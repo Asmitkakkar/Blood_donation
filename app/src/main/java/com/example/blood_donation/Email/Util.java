ea506e14c51339238047f57853d4a862193f2448
Asmitkakkar <asmitkakkar@gmail.com>
2022-08-07 19:22:40 +0530

    1st commit

---
+++ app/src/main/java/com/example/blood_donation/Email/Util.java
@@ -0,0 +1,7 @@
+package com.example.blood_donation.Email;
+
+public class Util {
+
+    public static final String EMAIL="asmitkakkar@gmail.com";
+    public static final String PASSWORD="yuhslmfznnnjejgc";
+}
